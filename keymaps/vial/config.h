bc234bd03a7e6923dd127a0a953daf1a09cdf565
goldstem <goldstem@oregonstate.edu>
2024-12-06 12:06:30 -0800

    Added completed files

---
+++ keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x1D, 0xE4, 0xF7, 0x2D, 0x69, 0xAE, 0x77, 0xDA}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
